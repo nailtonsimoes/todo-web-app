621cb1d6f5cfc25af1d9b777c1982f5ae193e422
Nailton Simoes <nailtonsimoes@gmail.com>
2023-01-25 03:11:33 -0300

    primeiro commit

---
+++ backend/src/test/java/com/naisilva/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naisilva.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
