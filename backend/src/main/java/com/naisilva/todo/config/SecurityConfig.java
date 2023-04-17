07c2ce32c07bb58c7bc449d05978b5e477ac41d0
Nailton Simões <nailtonsimoes@gmail.com>
2023-04-17 05:43:07 -0300

    remorção de imports nao usados em SecurityConfig

--- backend/src/main/java/com/naisilva/todo/config/SecurityConfig.java
+++ backend/src/main/java/com/naisilva/todo/config/SecurityConfig.java
@@ -1,12 +1,7 @@
 package com.naisilva.todo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -26,6 +21,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
-
 
 }
