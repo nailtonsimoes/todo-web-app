0eb8d2da7fa95935a6d06ef6155358ff21fea2d8
Nailton Simoes <nailton.silva@capgemini.com>
2023-05-03 14:00:35 -0300

    adição de validações em userRequestDto

--- backend/src/main/java/com/naisilva/todo/dtos/UserRequestDto.java
+++ backend/src/main/java/com/naisilva/todo/dtos/UserRequestDto.java
@@ -4,18 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequestDto {
-
-    @NotNull
+    @NotNull(message = "O campo nome é obrigatório.")
+    @Size(min = 2, max = 50, message = "O nome deve ter entre 2 e 50 caracteres.")
     private String name;
-    @NotNull
+
+    @NotNull(message = "O campo email é obrigatório.")
+    @Email(message = "O email deve ser válido.")
     private String email;
-    @NotNull
+
+    @NotNull(message = "O campo senha é obrigatório.")
+    @Size(min = 6, max = 20, message = "A senha deve ter entre 6 e 20 caracteres.")
     private String password;
 
     private Long roleId;
