c40ab6f2df05ba35644152be8d36f58186b551bb
Nailton Simoes <nailton.silva@capgemini.com>
2023-06-13 19:01:24 -0300

    identação de authguard

--- frontend/todo/src/app/components/auth/auth.guard.ts
+++ frontend/todo/src/app/components/auth/auth.guard.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { LoginService } from '../login/login.service';
-
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { LoginService } from "../login/login.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private router: Router, private loginService: LoginService) {
-  }
+  constructor(private router: Router, private loginService: LoginService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-
-    if(this.loginService.isUserLoggedIn()){
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.loginService.isUserLoggedIn()) {
       return true;
     } else {
-      this.router.navigate(['login']);
+      this.router.navigate(["login"]);
       return false;
     }
   }
-
 }
